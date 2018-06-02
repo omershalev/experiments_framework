e69076a65f11fe8b605215b5090b81f65d087ade
Omer Shalev <omershalva@gmail.com>
2018-06-02 13:36:41 +0300

    Added new experiments: icp, image_comparison, move_base and color_sampler

---
+++ content/experiments/icp.py
@@ -0,0 +1,56 @@
+import time
+import os
+
+import experiments_framework.framework.ros_utils as ros_utils
+import experiments_framework.framework.utils as utils
+import experiments_framework.framework.viz_utils as viz_utils
+from experiments_framework.framework.experiment import Experiment
+
+class Icp(Experiment):
+
+    def clean_env(self):
+        utils.kill_process('laser_scan_matcher_node')
+        ros_utils.kill_master()
+
+    def task(self, **kwargs):
+        ros_utils.start_master(use_sim_time=True)
+        ros_utils.launch(direct_path=r'/home/omer/orchards_ws/src/air_ground_orchard_navigation/scan_matcher.launch')
+        ros_utils.start_recording_bag(os.path.join(self.repetition_dir, self.name), ['/scanmatcher_pose',])
+        _, bag_duration = ros_utils.play_bag(self.data_sources, use_clock=True)
+
+        for _ in range(int(bag_duration)):
+            time.sleep(1)
+
+
+        # time.sleep(bag_duration)
+        ros_utils.stop_recording_bags()
+        time.sleep(1)
+
+
+class DrawIcp(Experiment):
+
+    def clean_env(self):
+        pass
+
+    def task(self):
+        icp = ros_utils.bag_to_dataframe(self.data_sources, topic=r'/scanmatcher_pose', fields = ['x', 'y'])
+        viz_utils.plot_2d_trajectory([icp], labels=['icp'], file_name=os.path.join(self.repetition_dir, 'icp'))
+
+if __name__ == '__main__':
+
+    import experiments_framework.content.data_pointers.lavi_april_18.jackal as jackal_data
+
+    bag_descriptor = jackal_data.jackal_18['15-11-01']
+    execution_dir = utils.create_new_execution_folder('icp_jackal_18')
+
+    # experiment = Icp(name='icp_test', data_sources=bag_descriptor.path, working_dir=execution_dir)
+    # experiment.run(repetitions=1)
+
+    experiment = DrawIcp(name='dummy', data_sources=r'/home/omer/orchards_ws/output/y20180531-173943_icp_jackal_18/20180531-173943_icp_test/0icp_test.bag', working_dir=execution_dir)
+    experiment.run(repetitions=1)
+
+    # TODO: check different combination: with/without imu and odometry
+    # TODO: check combinations in EKF (add icp as additional input, choose a few typical configurations)
+    # TODO: check for errors (final + waypoint)
+    # TODO: gradual plotting (just like in EKF)
+    # TODO: put this and EKF on the same plot
---
+++ content/experiments/move_base.py
@@ -0,0 +1,27 @@
+import cv2
+
+import experiments_framework.framework.viz_utils as viz_utils
+import air_ground_orchard_navigation.computer_vision.image_alignment as align
+
+if __name__ == '__main__':
+
+    import experiments_framework.content.data_pointers.lavi_april_18.dji as dji_data
+
+    obstacle = dji_data.snapshots_80_meters['15-17-1']
+    clear = dji_data.snapshots_80_meters['15-10-1']
+
+    img_obstacle = cv2.imread(obstacle.path)
+    img_clear = cv2.imread(clear.path)
+    img_obstacle_reg, h = align.register(img_obstacle, img_clear)
+    # viz_utils.show_image('imreg', img_obstacle_reg)
+    # img_diff = img_obstacle_reg - img_clear
+    img_gray_diff = cv2.subtract(cv2.cvtColor(img_obstacle_reg, cv2.COLOR_BGR2GRAY), cv2.cvtColor(img_clear, cv2.COLOR_BGR2GRAY))
+    viz_utils.show_image('imreg_diff', img_gray_diff)
+    # img_diff_denoised = cv2.fastNlMeansDenoising(img_gray_diff, None, 10,10,7)
+    # viz_utils.show_image('imreg_denoise', img_diff_denoised)
+    # thresh = cv2.adaptiveThreshold(img_gray_diff, 0, adaptiveMethod=cv2.ADAPTIVE_THRESH_MEAN_C, thresholdType=cv2.THRESH_BINARY_INV, blockSize=21, C=2)
+    # viz_utils.show_image('imreg_denoise', thresh)
+
+    se = cv2.getStructuringElement(cv2.MORPH_RECT, (15,15))
+    out = cv2.morphologyEx(img_gray_diff, cv2.MORPH_CLOSE, se)
+    viz_utils.show_image('imreg_denoise', out)
