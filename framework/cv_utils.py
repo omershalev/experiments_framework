ade6403092ff4a04fbb2769d9160c7a912e46cd1
Omer Shalev <omershalva@gmail.com>
2018-07-21 16:59:38 +0300

    New drawing rectangles

--- framework/cv_utils.py
+++ framework/cv_utils.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import datetime
+import time
 
 import viz_utils
 
@@ -8,11 +9,11 @@ import viz_utils
 def sample_pixel_coordinates(image):
     class _CoordinatesSampler(object):
         def __init__(self):
-            self.sampled_x = None
-            self.sampled_y = None
+            self.clicked_x = None
+            self.clicked_y = None
         def mouse_callback(self, event, x, y, flags, param):
             if event == cv2.EVENT_LBUTTONDOWN:
-                self.sampled_x, self.sampled_y = x, y
+                self.clicked_x, self.clicked_y = x, y
 
     cs = _CoordinatesSampler()
     viz_utils.show_image('image', image, wait_key=False)
@@ -21,7 +22,8 @@ def sample_pixel_coordinates(image):
         key = cv2.waitKey(1) & 0xFF
         if key == ord('q'):
             break
-    return cs.sampled_x, cs.sampled_y
+    cv2.destroyWindow('image')
+    return cs.clicked_x, cs.clicked_y
 
 
 def sample_hsv_color(image):
@@ -41,7 +43,6 @@ def sample_hsv_color(image):
     return sampled_pixels
 
 
-
 def mark_trajectory_on_image(image):
     class _TrajectoryMarker(object):
         def __init__(self, image):
@@ -55,8 +56,9 @@ def mark_trajectory_on_image(image):
 
         def mouse_callback(self, event, x, y, flags, param):
             if self.mark:
-                self.pose_time_tuples_list.append((x, y, datetime.datetime.now()))
-                for (x, y) in self.pose_time_tuples_list:
+                # self.pose_time_tuples_list.append((x, y, datetime.datetime.now())) # TODO: remove
+                self.pose_time_tuples_list.append((x, y, time.time()))
+                for (x, y, _) in self.pose_time_tuples_list:
                     self.image = cv2.circle(img=self.image, center=(x, y), radius=2, color=(255, 255, 255),
                                             thickness=-1)
                 viz_utils.show_image('image', self.image, wait_key=False)
@@ -73,3 +75,24 @@ def mark_trajectory_on_image(image):
         if key == ord('q'):
             break
     return tm.pose_time_tuples_list
+
+
+def mark_rectangle_on_image(image, points):
+    image_with_rectangle = image.copy()
+    for points_pair in zip(points[-1:] + points[:-1], points):
+        image_with_rectangle = cv2.line(image_with_rectangle, tuple(points_pair[0]), tuple(points_pair[1]), color=(0, 255, 0), thickness=2)
+    return image_with_rectangle
+
+
+def get_bounding_box(image, points, expand_ratio=0.0):
+    x, y, w, h = cv2.boundingRect(np.array(points))
+    upper_left_x = max(0, x - int(expand_ratio*w))
+    upper_left_y = max(0, y - int(expand_ratio*h))
+    lower_right_x = min(image.shape[1], x + int((1+expand_ratio)*w))
+    lower_right_y = min(image.shape[0], y + int((1+expand_ratio)*h))
+    return (upper_left_x, upper_left_y), (lower_right_x, lower_right_y)
+
+
+def mark_bounding_box(image, points, expand_ratio=0.0):
+    (upper_left_x, upper_left_y), (lower_right_x, lower_right_y) = get_bounding_box(image, points, expand_ratio)
+    cv2.rectangle(image, (upper_left_x, upper_left_y), (lower_right_x, lower_right_y), (255, 255, 0), 2)
