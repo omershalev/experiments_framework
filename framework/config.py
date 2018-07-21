bbb2d80c303cafdfc3d7e8ceb24d0cb210c8b768
Omer Shalev <omershalva@gmail.com>
2018-07-21 16:58:34 +0300

    Added marker locations to config

--- framework/config.py
+++ framework/config.py
@@ -1,5 +1,6 @@
 base_raw_data_path = r'/home/omer/orchards_ws/data/lavi_apr_18/raw'
 panorama_path = r'/home/omer/orchards_ws/data/lavi_apr_18/panorama'
+markers_locations_path = r'/home/omer/orchards_ws/data/lavi_apr_18/markers_locations'
 
 base_output_path = r'/home/omer/orchards_ws/output'
 
