edeca103ec926e79348153a187802d5085fb6fc1
Omer Shalev <omershalva@gmail.com>
2018-09-11 13:26:14 +0300

    Added scan parameters to config

--- framework/config.py
+++ framework/config.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 
 ############################################# PATHS #############################################
 root_dir_path = r'/home/omer/orchards_ws'
@@ -15,3 +16,8 @@ screen_resolution = (1920, 1080)
 markers_bounding_box_expand_ratio = 0.15
 synthetic_scan_target_frequency = 30
 top_view_resolution = 0.0125
+min_scan_angle = 0
+max_scan_angle = 2 * np.pi
+scan_samples_num = 360
+min_scan_distance = 3
+max_scan_distance = 300
