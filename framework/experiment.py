2f9e055e16c815f819edb7b750b47c45dede27e6
Omer Shalev <omershalva@gmail.com>
2018-09-19 11:24:26 +0300

    Serialization for numpy array objects before dumping to json

--- framework/experiment.py
+++ framework/experiment.py
@@ -6,6 +6,7 @@ import json
 from collections import OrderedDict
 
 import logger
+import numpy as np
 
 _logger = logger.get_logger()
 
@@ -33,24 +34,31 @@ class Experiment(object):
             experiment_dir_name = '%s_%s' % (datetime.datetime.now().strftime('%Y%m%d-%H%M%S'), self.name)
             self.experiment_dir = os.path.join(self.working_dir, experiment_dir_name)
             os.mkdir(self.experiment_dir)
-            i = 0
-            while i < repetitions:
-                self.repetition_dir = os.path.join(self.experiment_dir, str(i))
+            self.repetition_id = 1
+            while self.repetition_id <= repetitions:
+                self.results[self.repetition_id] = {}
+                self.repetition_dir = os.path.join(self.experiment_dir, str(self.repetition_id))
                 os.mkdir(self.repetition_dir)
-                _logger.info('Beginning repetition #%d' % i)
+                _logger.info('Beginning repetition #%d' % self.repetition_id)
                 self.clean_env()
                 self.task(**kwargs)
                 if self.is_valid():
                     _logger.info('Valid execution')
-                    i += 1
+                    self.repetition_id += 1
                 else:
                     _logger.info('Invalid execution')
                     os.rename(self.repetition_dir, '%s_invalid' % self.repetition_dir)
-                    i += 1
+                    self.repetition_id += 1
+            def make_serializable(value):
+                if type(value) is np.ndarray:
+                    return value.tolist()
+                return value
+            params_to_dump = {key: make_serializable(value) for key, value in self.params.items()}
+            results_to_dump = {key: make_serializable(value) for key, value in self.results.items()}
             json_obj = {'name': self.name,
                         'data_sources': self.data_sources,
-                        'params': self.params,
-                        'results': self.results}
+                        'params': params_to_dump,
+                        'results': results_to_dump}
             with open(os.path.join(self.experiment_dir, 'experiment_metadata.json'), 'w') as json_file:
                 json.dump(json_obj, json_file, indent=4)
         finally:
