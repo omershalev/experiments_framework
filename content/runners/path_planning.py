31ee7341b04894b9ff474000191bcb723d523ef6
Omer Shalev <omershalva@gmail.com>
2018-09-23 16:13:22 +0300

    Stable version of path planning runner

--- content/runners/path_planning.py
+++ content/runners/path_planning.py
@@ -2,13 +2,17 @@ import os
 import json
 import numpy as np
 import random
+
+from computer_vision import calibration
 from framework import utils
+from framework import config
 from content.data_pointers.lavi_april_18 import orchard_topology
 from content.experiments.path_planning import PathPlanningExperiment
 from content.data_pointers.lavi_april_18.dji import trunks_detection_results_dir, selected_trunks_detection_experiments_and_repetitions
 
 num_of_trajectories = 20
-trunk_radius = 30 # TODO: change according to measurements
+
+# TODO: play with hyper paremeters (sigma, radius, etc.) and show their influence on the chosen trajectories - those are parameters configurable by the farmer!!!
 
 if __name__ == '__main__':
     execution_dir = utils.create_new_execution_folder('path_planning')
@@ -17,20 +21,32 @@ if __name__ == '__main__':
         trunks_detection_summary = json.load(f)
     image_path = trunks_detection_summary['data_sources']
     image_key = trunks_detection_summary['metadata']['image_key']
-    trunk_points_list = trunks_detection_summary['results'][str(trunks_detection_repetition)]['pattern_points']
+    optimized_sigma = trunks_detection_summary['results'][str(trunks_detection_repetition)]['optimized_sigma']
+    optimized_grid_dim_x_in_pixels = trunks_detection_summary['results'][str(trunks_detection_repetition)]['optimized_grid_dim_x']
+    optimized_grid_dim_y_in_pixels = trunks_detection_summary['results'][str(trunks_detection_repetition)]['optimized_grid_dim_y']
+    trunk_radius = calibration.calculate_average_trunk_radius_in_pixels(optimized_grid_dim_x_in_pixels, optimized_grid_dim_y_in_pixels,
+                                                                        orchard_topology.measured_row_widths, orchard_topology.measured_intra_row_distances,
+                                                                        orchard_topology.measured_trunks_perimeters, config.trunk_dilation_ratio)
+    semantic_trunks = trunks_detection_summary['results'][str(trunks_detection_repetition)]['semantic_trunks']
+    trunk_points_list = semantic_trunks.values()
     orchard_pattern = orchard_topology.orchard_pattern
+    row_labels = np.unique([label.split('/')[0] for label in semantic_trunks.keys()])
+    tree_labels = np.unique([label.split('/')[1] for label in semantic_trunks.keys()])
     i = 0
-
     while i < num_of_trajectories:
-        start_index = random.randint(0, len(trunk_points_list) - 1)
-        goal_index = random.randint(0, len(trunk_points_list) - 1)
-        if start_index == goal_index:
+        start_label = '%s/%s' % (np.random.choice(row_labels), np.random.choice(tree_labels))
+        goal_label = '%s/%s' % (np.random.choice(row_labels), np.random.choice(tree_labels))
+        if start_label == goal_label:
+            continue
+        if start_label not in semantic_trunks.keys() or goal_label not in semantic_trunks.keys():
             continue
         i += 1
-        start = (int(np.round(trunk_points_list[start_index][0] + trunk_radius + 5)), int(np.round(trunk_points_list[start_index][1])))
-        goal = (int(np.round(trunk_points_list[goal_index][0] + trunk_radius + 5)), int(np.round(trunk_points_list[goal_index][1])))
-        experiment = PathPlanningExperiment(name='path_planning_on_%s_from_%s_to_%s' % (image_key, start, goal),
-                                            data_sources={'image_path': image_path, 'trunk_points_list': trunk_points_list, 'trunk_radius': trunk_radius, # TODO: change 30
-                                                          'canopy_sigma': 150, 'start': start, 'goal': goal}, # TODO: change 100
-                                            working_dir=execution_dir, metadata={'image_key': image_key, 'altitude': 80})
+        start = tuple(np.array(semantic_trunks[start_label]) + np.array([trunk_radius + 5, 0]))
+        goal = tuple(np.array(semantic_trunks[goal_label]) + np.array([trunk_radius + 5, 0]))
+        waypoints = [start, goal]
+        experiment = PathPlanningExperiment(name='path_planning_on_%s_from_%s_to_%s' % (image_key, start_label.replace('/', ''), goal_label.replace('/', '')), # TODO: remove hardcoded
+                                            data_sources={'image_path': image_path, 'trunk_points_list': trunk_points_list, 'waypoints': waypoints},
+                                            params={'trunk_radius': trunk_radius, 'gaussian_scale_factor': config.cost_map_gaussians_scale_factor,
+                                                    'canopy_sigma': optimized_sigma, 'bounding_box_expand_ratio': config.bounding_box_expand_ratio},
+                                            working_dir=execution_dir, metadata=trunks_detection_summary['metadata'])
         experiment.run(repetitions=1)
