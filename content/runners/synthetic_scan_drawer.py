063147c2f25d4b2ea1fa9fd9505b4298612ec2a5
Omer Shalev <omershalva@gmail.com>
2018-10-01 10:20:36 +0300

    Refactor

--- content/runners/synthetic_scan_drawer.py
+++ content/runners/synthetic_scan_drawer.py
@@ -2,13 +2,14 @@ import cv2
 import numpy as np
 
 from air_ground_orchard_navigation.computer_vision import segmentation, contours_scan2
-import sys
-sys.path.append(r'/home/omer/orchards_ws/src/air_ground_orchard_navigation/computer_vision/src/air_ground_orchard_navigation/computer_vision')
-import contours_scan
-
-from experiments_framework.content.data_pointers.lavi_april_18 import panorama
-from experiments_framework.framework import viz_utils
-from experiments_framework.framework import cv_utils
+# import sys
+# sys.path.append(r'/home/omer/orchards_ws/astar/air_ground_orchard_navigation/computer_vision/astar/air_ground_orchard_navigation/computer_vision')
+# import contours_scan
+from air_ground_orchard_navigation.computer_vision.contours_scan_cython import contours_scan
+from framework import config
+from content.data_pointers.lavi_april_18 import panorama
+from framework import viz_utils
+from framework import cv_utils
 
 
 if __name__ == '__main__':
@@ -19,11 +20,11 @@ if __name__ == '__main__':
     x, y = cv_utils.sample_pixel_coordinates(map_image)
     import datetime
     start1 = datetime.datetime.now()
-    range1, coordinates_list = contours_scan2.generate(map_image, center_x=x, center_y=y, min_angle=0, max_angle=2 * np.pi, samples_num=360, min_distance=3, max_distance=300, resolution=0.0125) # TODO: resolution
+    range1, coordinates_list = contours_scan2.generate(map_image, center_x=x, center_y=y, min_angle=0, max_angle=2 * np.pi, samples_num=360, min_distance=3, max_distance=300, resolution=config.top_view_resolution) # TODO: resolution
     end1 = datetime.datetime.now()
     print 'without: ' + str((end1 - start1).microseconds)
     start2 = datetime.datetime.now()
-    range2 = contours_scan.generate(map_image, center_x=x, center_y=y, min_angle=0, max_angle=2 * np.pi, samples_num=360, min_distance=3, max_distance=300, resolution=0.0125) # TODO: resolution
+    range2 = contours_scan.generate(map_image, center_x=x, center_y=y, min_angle=0, max_angle=2 * np.pi, samples_num=360, min_distance=3, max_distance=300, resolution=config.top_view_resolution) # TODO: resolution
     end2 = datetime.datetime.now()
     print 'with: ' + str((end2 - start2).microseconds)
     map_image = cv2.cvtColor(map_image, cv2.COLOR_GRAY2RGB)
