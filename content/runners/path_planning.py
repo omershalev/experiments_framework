73f306863e6a39ec9bbadf82511de81374945cef
Omer Shalev <omershalva@gmail.com>
2018-12-29 15:25:26 +0200

    Minor changes

--- content/runners/path_planning.py
+++ content/runners/path_planning.py
@@ -21,8 +21,6 @@ setup = 'apr' # apr / nov1 / nov2 / nov3 / nov4
 
 if setup == 'apr':
     from content.data_pointers.lavi_april_18.dji import trunks_detection_results_dir as td_results_dir
-    from content.data_pointers.lavi_april_18.dji import selected_trunks_detection_experiments as selected_td_experiments
-    from content.data_pointers.lavi_april_18 import orchard_topology
 elif setup == 'nov1':
     raise NotImplementedError # TODO: implement
 elif setup == 'nov2':
@@ -35,8 +33,8 @@ elif setup == 'nov4':
 
 if __name__ == '__main__':
     execution_dir = utils.create_new_execution_folder(description)
-    trunks_detection_experiment_name = selected_td_experiments[0]
-    with open(os.path.join(td_results_dir, trunks_detection_experiment_name, 'experiment_summary.json')) as f:
+    td_experiment_name = 'manual_apr_15-08-1'
+    with open(os.path.join(td_results_dir, td_experiment_name, 'experiment_summary.json')) as f:
         trunks_detection_summary = json.load(f)
     image_path = trunks_detection_summary['data_sources']
     image_key = trunks_detection_summary['metadata']['image_key']
@@ -44,7 +42,6 @@ if __name__ == '__main__':
     trunk_points_list = semantic_trunks.values()
     image = cv2.imread(image_path)
     upper_left, lower_right = cv_utils.get_bounding_box(image, trunk_points_list, expand_ratio=config.bounding_box_expand_ratio)
-    orchard_pattern = orchard_topology.plot_pattern
     row_labels = np.unique([label.split('/')[0] for label in semantic_trunks.keys()])
     tree_labels = np.unique([label.split('/')[1] for label in semantic_trunks.keys()])
     i = 0
@@ -69,19 +66,28 @@ if __name__ == '__main__':
                                                                                               start_label_2.replace('/', ''),
                                                                                               goal_label_1.replace('/', ''),
                                                                                               goal_label_2.replace('/', '')),
-                                            data_sources={'map_image_path': image_path, 'trunk_points_list': trunk_points_list,
-                                                          'map_upper_left': upper_left, 'map_lower_right': lower_right, 'waypoints': waypoints},
+                                            data_sources={'map_image_path': image_path, 'map_upper_left': upper_left,
+                                                          'map_lower_right': lower_right, 'waypoints': waypoints},
                                             working_dir=execution_dir, metadata=trunks_detection_summary['metadata'])
         experiment.run(repetitions=1)
-        trajectory_image = cv2.imread(experiment.results[1]['trajectory_image_path'])
+
         selected_trunks = {label: semantic_trunks[label] for label in [start_label_1, start_label_2, goal_label_1, goal_label_2]}
-        trajectory_image = cv_utils.draw_points_on_image(trajectory_image,
-                                                         [np.array(selected_trunks[trunk_label]) - np.array(upper_left) for trunk_label in selected_trunks.keys()],
-                                                         color=(0, 255, 0), radius=20)
+        trajectory_on_cost_map_image = cv2.imread(experiment.results[1]['trajectory_on_cost_map_path'])
+        trajectory_on_cost_map_image = cv_utils.draw_points_on_image(trajectory_on_cost_map_image,
+                                                                     [np.array(selected_trunks[trunk_label]) - np.array(upper_left) for trunk_label in selected_trunks.keys()],
+                                                                     color=(0, 255, 0), radius=20)
+        trajectory_on_image = cv2.imread(experiment.results[1]['trajectory_on_image_path'])
+        trajectory_on_image = cv_utils.draw_points_on_image(trajectory_on_image,
+                                                            [np.array(selected_trunks[trunk_label]) - np.array(upper_left) for trunk_label in selected_trunks.keys()],
+                                                            color=(0, 255, 0), radius=20)
         for trunk_label in selected_trunks.keys():
-            cv2.putText(trajectory_image, trunk_label, tuple(np.array(semantic_trunks[trunk_label]) - np.array(upper_left) + np.array([-38, -68])),
-                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2, color=(30, 30, 30), thickness=10, lineType=cv2.LINE_AA)
-            cv2.putText(trajectory_image, trunk_label, tuple(np.array(semantic_trunks[trunk_label]) - np.array(upper_left) + np.array([-40, -70])),
-                        fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=2, color=(0, 255, 0), thickness=10, lineType=cv2.LINE_AA)
-        cv2.imwrite(os.path.join(experiment.experiment_dir, 'trajectory_with_labels.jpg'), trajectory_image)
+            label_location = tuple(np.array(semantic_trunks[trunk_label]) - np.array(upper_left))
+            trajectory_on_cost_map_image = cv_utils.put_shaded_text_on_image(trajectory_on_cost_map_image, trunk_label, label_location, color=(0, 255, 0))
+            trajectory_on_image = cv_utils.put_shaded_text_on_image(trajectory_on_image, trunk_label, label_location, color=(0, 255, 0))
+        start_goal_str = '%s-%s_to_%s-%s' % (start_label_1.replace('/', ''),
+                                             start_label_2.replace('/', ''),
+                                             goal_label_1.replace('/', ''),
+                                             goal_label_2.replace('/', '')),
+        cv2.imwrite(os.path.join(execution_dir, 'trajectory_on_cost_map_%s.jpg' % start_goal_str), trajectory_on_cost_map_image)
+        cv2.imwrite(os.path.join(execution_dir, 'trajectory_on_colored_image_%s.jpg' % start_goal_str), trajectory_on_image)
         i += 1
