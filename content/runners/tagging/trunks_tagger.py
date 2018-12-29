1825e42ffa027fd136d374f3f6dd5f6da2680023
Omer Shalev <omershalva@gmail.com>
2018-12-29 09:43:21 +0200

    Modified trunks tagger to produce the standard json file

--- content/runners/tagging/trunks_tagger.py
+++ content/runners/tagging/trunks_tagger.py
@@ -2,47 +2,65 @@ import os
 import cv2
 import json
 
-from experiments_framework.framework import cv_utils
-from experiments_framework.framework import utils
-from experiments_framework.framework import viz_utils
-from air_ground_orchard_navigation.computer_vision import segmentation
-from experiments_framework.content.data_pointers.lavi_april_18 import dji
-from experiments_framework.framework import config
-from experiments_framework.framework import logger
+from framework import cv_utils
+from framework import utils
 
-_logger = logger.get_logger()
 
-image_path = None # ignored if None
-relevant_keys = None # if None, take all
-snapshots_groups = [dji.snapshots_60_meters, dji.snapshots_80_meters]
-markers_locations_json_paths = [dji.snapshots_60_meters_markers_locations_json_path, dji.snapshots_80_meters_markers_locations_json_path]
-fork_shaped = True
-S_shaped = True
-random_shaped = True
+#################################################################################################
+#                                             CONFIG                                            #
+#################################################################################################
+altitude = 80
+repetitions = 1
+grid_size_values = [6]
 
+setup = 'apr' # apr / nov1 / nov2 / nov3 / nov4
+#################################################################################################
+
+if setup == 'apr':
+    from content.data_pointers.lavi_april_18.dji import snapshots_80_meters as snapshots
+    from content.data_pointers.lavi_april_18.orchard_topology import plot_pattern as plot_pattern
+elif setup == 'nov1':
+    from content.data_pointers.lavi_november_18.dji import plot1_snapshots_80_meters as snapshots
+    from content.data_pointers.lavi_november_18.orchard_topology import plot1_pattern as plot_pattern
+elif setup == 'nov2':
+    from content.data_pointers.lavi_november_18.dji import plot2_snapshots_80_meters as snapshots
+    from content.data_pointers.lavi_november_18.orchard_topology import plot2_pattern as plot_pattern
+elif setup == 'nov3':
+    from content.data_pointers.lavi_november_18.dji import plot3_snapshots_80_meters as snapshots
+    from content.data_pointers.lavi_november_18.orchard_topology import plot3_pattern as plot_pattern
+elif setup == 'nov4':
+    from content.data_pointers.lavi_november_18.dji import plot4_snapshots_80_meters as snapshots
+    from content.data_pointers.lavi_november_18.orchard_topology import plot4_pattern as plot_pattern
+
+
+image_keys = ['16-55-1', '19-04-1']
 
 if __name__ == '__main__':
-
-    if image_path is not None:
-        raise NotImplemented
-    group_idx = 1
-    for snapshots_group, markers_locations_json_path in zip(snapshots_groups, markers_locations_json_paths):
-        execution_dir = utils.create_new_execution_folder('trunks_tagging_%d' % group_idx)
-        group_idx += 1
-        with open(markers_locations_json_path) as f:
-            markers_locations = json.load(f)
-        for key, data_descriptor in snapshots_group.items():
-            if relevant_keys is not None:
-                if key not in relevant_keys:
+    execution_dir = utils.create_new_execution_folder('trunks_tagging')
+    for image_key in image_keys:
+        summary = {}
+        summary['results'] = {}
+        summary['results'][0] = {}
+        summary['results'][0]['semantic_trunks'] = {}
+        summary['metadata'] = {}
+        summary['metadata']['image_key'] = image_key
+        summary['metadata']['altitude'] = altitude
+        data_descriptor = snapshots[image_key]
+        summary['data_sources'] = data_descriptor.path
+        image = cv2.imread(data_descriptor.path)
+        for i in range(plot_pattern.shape[0]):
+            for j in range(plot_pattern.shape[1]):
+                if plot_pattern[(i, j)] == -1:
                     continue
-            points = markers_locations[key]
-            image = cv2.imread(data_descriptor.path)
-            map_image = segmentation.extract_canopies_map(image)
-            (upper_left_x, upper_left_y), (lower_right_x, lower_right_y) = cv_utils.get_bounding_box(map_image, points, expand_ratio=config.markers_bounding_box_expand_ratio)
-            map_image = map_image[upper_left_y:lower_right_y, upper_left_x:lower_right_x]
-            trunk_poses = cv_utils.sample_pixel_coordinates(map_image, multiple=True)
-            for pose in trunk_poses:
-                cv2.circle(map_image, pose, radius=15, color=0, thickness=-1)
-            viz_utils.show_image('trunk_locations', map_image)
-            with open(os.path.join(execution_dir, '%s_trunks.json' % key), 'w') as f:
-                json.dump(trunk_poses, f, indent=4)
+                tree_label = '%d/%s' % (j + 1, chr(65 + (plot_pattern.shape[0] - 1 - i)))
+                trunk_pose = cv_utils.sample_pixel_coordinates(image)
+                summary['results'][0]['semantic_trunks'][tree_label] = trunk_pose
+                image = cv_utils.draw_points_on_image(image, [trunk_pose], color=(255, 255, 255))
+                cv2.putText(image, tree_label, trunk_pose, fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                            fontScale=2, color=(255, 255, 255), thickness=8, lineType=cv2.LINE_AA)
+        # typical values
+        summary['results'][0]['optimized_grid_dim_x'] = 270
+        summary['results'][0]['optimized_grid_dim_y'] = 230
+        summary['results'][0]['optimized_sigma'] = 80
+        with open(os.path.join(execution_dir, '%s_trunks.json' % image_key), 'w') as f:
+            json.dump(summary, f, indent=4)
