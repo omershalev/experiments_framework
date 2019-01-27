51461e27b857bbdc77092b268ed1ad8bcd5fc09e
Omer Shalev <omershalva@gmail.com>
2019-01-27 15:20:02 +0200

    Updated contours drawer

--- content/runners/contours_drawer.py
+++ content/runners/contours_drawer.py
@@ -1,33 +1,38 @@
 import os
 import cv2
-import numpy as np
+import json
 
 from computer_vision import segmentation
-import framework.viz_utils as viz_utils
-import framework.utils as utils
-import content.data_pointers.lavi_april_18.dji as dji_data
+from framework import utils
 
-image_paths_list = [descriptor.path for descriptor in dji_data.snapshots_60_meters.values() + dji_data.snapshots_80_meters.values()]
-show = True
-image_path = r'/home/omer/orchards_ws/data/lavi_apr_18/panorama/DJI_0178_afternoon_good_stitch_full_movie.jpg' # ignored if None
+#################################################################################################
+#                                             CONFIG                                            #
+#################################################################################################
+source_image_index = 2
+setup = 'nov1' # apr / nov1
+#################################################################################################
+
+if setup == 'apr':
+    from content.data_pointers.lavi_april_18.dji import trunks_detection_results_dir as td_results_dir
+    from content.data_pointers.lavi_april_18.dji import selected_trunks_detection_experiments as selected_td_experiments
+elif setup == 'nov1':
+    from content.data_pointers.lavi_november_18.dji import trunks_detection_results_dir as td_results_dir
+    from content.data_pointers.lavi_november_18.dji import selected_trunks_detection_experiments as selected_td_experiments
+else:
+    raise NotImplementedError
 
 if __name__ == '__main__':
-    execution_dir = utils.create_new_execution_folder('canopy_contours_images')
-    if image_path is not None:
-        image = cv2.imread(image_path)
-        contours, _ = segmentation.extract_canopy_contours(image)
-        cv2.drawContours(image, contours, contourIdx=-1, color=(0, 255, 0), thickness=3)
-        cv2.imwrite(os.path.join(execution_dir, 'image.jpg'), image)
-        if show:
-            viz_utils.show_image('image', image)
-    else:
-        idx = 0
-        for image_path in image_paths_list:
-            image = cv2.imread(image_path)
-            contours, _ = segmentation.extract_canopy_contours(image)
-            cv2.drawContours(image, contours, contourIdx=-1, color=(0, 255, 0), thickness=3)
-            cv2.imwrite(os.path.join(execution_dir, '%s.jpg' % idx), image)
-            idx += 1
+    execution_dir = utils.create_new_execution_folder('canopy_contours_drawer')
+    with open(os.path.join(td_results_dir, selected_td_experiments[source_image_index], 'experiment_summary.json')) as f:
+        td_summary = json.load(f)
+    image = cv2.imread(td_summary['data_sources'])
+    contours, canopies_mask = segmentation.extract_canopy_contours(image)
+    image_with_contours = image.copy()
+    cv2.drawContours(image_with_contours, contours, contourIdx=-1, color=(0, 255, 0), thickness=5)
+    canopies_mask_with_contours = cv2.cvtColor(canopies_mask.copy(), cv2.COLOR_GRAY2BGR)
+    cv2.drawContours(canopies_mask_with_contours, contours, contourIdx=-1, color=(0, 255, 0), thickness=5)
 
-            if show:
-                viz_utils.show_image('image', image)
+    cv2.imwrite(os.path.join(execution_dir, 'image.jpg'), image)
+    cv2.imwrite(os.path.join(execution_dir, 'canopies_mask.jpg'), canopies_mask)
+    cv2.imwrite(os.path.join(execution_dir, 'image_with_contours.jpg'), image_with_contours)
+    cv2.imwrite(os.path.join(execution_dir, 'canopies_mask_with_contours.jpg'), canopies_mask_with_contours)
