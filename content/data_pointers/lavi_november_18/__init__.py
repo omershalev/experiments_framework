c7d43e654bc85b1085b429703e2c06bae4eff13b
Omer Shalev <omershalva@gmail.com>
2019-02-02 20:49:13 +0200

    Fixed markers json paths

--- content/data_pointers/lavi_november_18/__init__.py
+++ content/data_pointers/lavi_november_18/__init__.py
@@ -3,4 +3,4 @@ import os
 
 base_resources_path = os.path.join(config.root_dir_path, 'resources/lavi_nov_18')
 base_raw_data_path = os.path.join(base_resources_path, 'raw')
-markers_locations_path = os.path.join(config.root_dir_path, 'markers_locations')
+markers_locations_path = os.path.join(base_resources_path, 'markers_locations')
