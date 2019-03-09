33034ed8a6b1b90d3ba22f04a1ae8680be1c3cfc
Omer Shalev <omershalva@gmail.com>
2019-03-09 11:30:01 +0200

    Updated comments

--- content/data_pointers/lavi_april_18/jackal.py
+++ content/data_pointers/lavi_april_18/jackal.py
@@ -5,14 +5,14 @@ from framework.data_descriptor import DataDescriptor
 base_path = lavi_april_18.base_raw_data_path
 
 jackal_18 = {'15-09-35': DataDescriptor(os.path.join(base_path, 'jackal_18', '2018-04-24-15-09-35_0.bag'), 'short warm up'),
-             '15-11-01': DataDescriptor(os.path.join(base_path, 'jackal_18', '2018-04-24-15-11-01_0.bag'), 'fork in rows 3-4, 4-5 and 5-6 ###############'),
-             '17-09-14': DataDescriptor(os.path.join(base_path, 'jackal_18', '2018-04-24-17-09-14_0.bag'), 'fork in rows 3-4, 4-5 and 5-6 (experiment 5.4) ###############'),
-             '17-18-53': DataDescriptor(os.path.join(base_path, 'jackal_18', '2018-04-24-17-18-53_0.bag'), 'fork in rows 3-4, 4-5 and 5-6 (experiment 5.4) ###############'),
-             '17-45-36': DataDescriptor(os.path.join(base_path, 'jackal_18', '2018-04-24-17-45-36_0.bag'), 'fork in rows 3-4, 4-5 and 5-6 (experiment 5.4) ###############'),
-             '17-58-58': DataDescriptor(os.path.join(base_path, 'jackal_18', '2018-04-24-17-58-58_0.bag'), 'random trajectory ###############'),
+             '15-11-01': DataDescriptor(os.path.join(base_path, 'jackal_18', '2018-04-24-15-11-01_0.bag'), 'fork in rows 3-4, 4-5 and 5-6'),
+             '17-09-14': DataDescriptor(os.path.join(base_path, 'jackal_18', '2018-04-24-17-09-14_0.bag'), 'fork in rows 3-4, 4-5 and 5-6 (experiment 5.4)'),
+             '17-18-53': DataDescriptor(os.path.join(base_path, 'jackal_18', '2018-04-24-17-18-53_0.bag'), 'fork in rows 3-4, 4-5 and 5-6 (experiment 5.4)'),
+             '17-45-36': DataDescriptor(os.path.join(base_path, 'jackal_18', '2018-04-24-17-45-36_0.bag'), 'fork in rows 3-4, 4-5 and 5-6 (experiment 5.4)'),
+             '17-58-58': DataDescriptor(os.path.join(base_path, 'jackal_18', '2018-04-24-17-58-58_0.bag'), 'random trajectory'),
              '18-24-26': DataDescriptor((os.path.join(base_path, 'jackal_18', '2018-04-24-18-24-26_0.bag'),
                                          os.path.join(base_path, 'jackal_18', '2018-04-24-18-28-23_1.bag'),
-                                         os.path.join(base_path, 'jackal_18', '2018-04-24-18-30-40_2.bag')), 'mapping ###############')}
+                                         os.path.join(base_path, 'jackal_18', '2018-04-24-18-30-40_2.bag')), 'mapping')}
 
 jackal_19 = [
             ]
