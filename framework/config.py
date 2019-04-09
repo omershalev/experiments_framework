88fe2db31b2f4cb2e60f1d638bdddce3b05d633a
Omer Shalev <omershalva@gmail.com>
2019-04-09 18:56:48 +0300

    Removed TODOs and changed scan radius to 250

--- framework/config.py
+++ framework/config.py
@@ -14,16 +14,15 @@ screen_resolution = (1920, 1080)
 #################################################################################################
 #                                             PARAMS                                            #
 #################################################################################################
-bounding_box_expand_ratio = 0.15 # TODO: this was changed from 0.15 - is that okay?
-target_system_frequency = 30 # TODO: consider 25...
+bounding_box_expand_ratio = 0.15
+target_system_frequency = 30
 top_view_resolution = 0.0125 # TODO: eventually, this should be removed
 synthetic_scan_min_angle = -np.pi
 synthetic_scan_max_angle = np.pi
 synthetic_scan_samples_num = 360
-synthetic_scan_min_distance = 3 # TODO: this is in pixels - problematic!
-synthetic_scan_max_distance = 300 # TODO: this is in pixels - problematic!; consider taking 350 pixels instead!!!!!
+synthetic_scan_min_distance = 3
+synthetic_scan_max_distance = 250
 synthetic_scan_r_primary_search_samples = 50
 synthetic_scan_r_secondary_search_step = 2
 trunk_dilation_ratio = 1
 trunk_std_increasing_factor = 2 # TODO: think
-cost_map_gaussians_scale_factor = 0.9
